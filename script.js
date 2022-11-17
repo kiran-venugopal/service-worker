c461474c2927d8fdb608e671a6ce26edf7008deb
Kiran V <kiranvenugopal07@gmail.com>
2022-11-17 21:36:22 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+async function registerSW() {
+  if ("serviceWorker" in navigator) {
+    const registration = await navigator.serviceWorker.register("/sw.js", {
+      scope: "/",
+    });
+
+    if (registration.installing) {
+      console.log("Service worker installing");
+    } else if (registration.waiting) {
+      console.log("Service worker installed");
+    } else if (registration.active) {
+      console.log("Service worker active");
+    }
+  }
+}
+
+registerSW();
---
+++ sw.js
@@ -0,0 +1,49 @@
+/**
+ * TODO:
+ *  - cache files in background after install stage
+ *  - update cache
+ *  - update SW
+ */
+
+const cacheName = "hello-pwa";
+const filesToCache = ["/", "/index.html", "/styles.css", "/script.js"];
+
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open("v1");
+  await cache.addAll(resources);
+};
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener("install", (event) => {
+  console.log(self);
+  event.waitUntil(addResourcesToCache(filesToCache));
+  console.log(self.navigator);
+});
+
+self.addEventListener("activate", function () {
+  console.log("activated!");
+});
+
+/* Serve cached content when offline */
+self.addEventListener("fetch", function (e) {
+  console.log({ url: e.request.url });
+  if (e.request.url === "http://127.0.0.1:5500/offline") {
+    e.respondWith(
+      new Response(
+        "<p>Hello from your friendly neighborhood service worker!</p>",
+        {
+          headers: { "Content-Type": "text/html" },
+        }
+      )
+    );
+  }
+  e.respondWith(
+    caches.match(e.request).then(async function (response) {
+      if (!response) {
+        await addResourcesToCache([e.request.url]);
+        return fetch(e.request);
+      }
+      return response;
+    })
+  );
+});
